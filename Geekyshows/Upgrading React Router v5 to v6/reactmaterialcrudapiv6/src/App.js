fe3feab94a5084ae495ad3ea1f438a27f62eea37
satyam-seth <satyam1998.1998@gmail.com>
2022-01-09 17:29:39 +0530

    third commit

---
+++ Geekyshows/Upgrading React Router v5 to v6/reactmaterialcrudapiv6/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import View from "./components/student/View";
+import Edit from "./components/student/Edit";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/view/:id" element={<View/>} />
+          <Route exact path="/edit/:id" element={<Edit/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
