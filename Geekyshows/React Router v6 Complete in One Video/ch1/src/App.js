e097efe26fcf8f6c5cb34d02a54a70a7e08a232e
satyam-seth <satyam1998.1998@gmail.com>
2023-12-13 23:57:14 +0530

    add route for an element

--- Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Route path='/' element={<h1>Hello React Router</h1>} />
       </BrowserRouter>
     </>
   );
