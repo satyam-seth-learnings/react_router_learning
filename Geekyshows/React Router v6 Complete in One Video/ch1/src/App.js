59c88d291a69ad23cd0189b4180cc63819bd1407
satyam-seth <satyam1998.1998@gmail.com>
2023-12-13 23:57:47 +0530

    wrap route with routes

--- Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Route path='/' element={<h1>Hello React Router</h1>} />
+        <Routes>
+          <Route path='/' element={<h1>Hello React Router</h1>} />
+        </Routes>
       </BrowserRouter>
     </>
   );
