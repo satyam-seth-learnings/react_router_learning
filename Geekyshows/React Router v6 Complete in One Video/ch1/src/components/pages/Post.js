b04261512dd18f0fbe8b39fc01c8bb3dbdbb28d9
satyam-seth <satyam1998.1998@gmail.com>
2023-12-15 00:55:57 +0530

    add example of useSearchParams hook to get access query param variable values in component

--- Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Post.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Post.js
@@ -1,7 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
+
+// http://localhost:3000/post/mobile/1/?price=1000&sort=asc
 
 const Post = () => {
     const { category, id } = useParams();
+    let [searchParams, setSearchParams] = useSearchParams()
+
+    console.log(searchParams)
+    console.log(searchParams.get('price'));
+    console.log(searchParams.get('sort'));
 
     return (
         <>
