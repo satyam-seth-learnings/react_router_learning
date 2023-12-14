6a0102a2c190e40cfd9965402e716abfedb12861
satyam-seth <satyam1998.1998@gmail.com>
2023-12-15 00:13:01 +0530

    create navbar component

---
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/components/Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Navbar;
