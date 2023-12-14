19baab2204556cefb8078c0e32f90cdd1bbd4de7
satyam-seth <satyam1998.1998@gmail.com>
2023-12-15 00:09:46 +0530

    add route with components

--- Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<h1>Hello React Router</h1>} />
+          {/* <Route path='/' element={<h1>Hello React Router</h1>} /> */}
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
