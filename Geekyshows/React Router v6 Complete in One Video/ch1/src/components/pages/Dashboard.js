e40f368e43a6dc74e18be7be1498edd8faee3ed7
satyam-seth <satyam1998.1998@gmail.com>
2023-12-15 23:32:48 +0530

    add example of useLoacation to access state passed using useNavigate

--- Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Dashboard.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Dashboard.js
@@ -2,11 +2,14 @@ import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
     const navigate = useNavigate();
+    const data = {
+        'name': 'Sonam'
+    }
 
     return (
         <>
             <h1>Dashboard Page</h1>
-            <button onClick={() => navigate('/logout')}>Logout</button>
+            <button onClick={() => navigate('/logout', { state: data })}>Logout</button>
         </>
     );
 }
--- Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Logout.js
+++ Geekyshows/React Router v6 Complete in One Video/ch1/src/components/pages/Logout.js
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom";
+
 const Logout = () => {
+    const location = useLocation();
+
     return (
         <>
             <h1>Logout Page</h1>
+            <h2>{location.state.name ?? 'User'} Logged Out!!</h2>
         </>
     );
 }
