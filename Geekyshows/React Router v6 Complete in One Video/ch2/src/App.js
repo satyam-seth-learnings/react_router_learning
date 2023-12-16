d77a335803254b91ce5b9e053153b5ee30f518a1
satyam-seth <satyam1998.1998@gmail.com>
2023-12-16 23:30:09 +0530

    add example of nested layout

--- Geekyshows/React Router v6 Complete in One Video/ch2/src/App.js
+++ Geekyshows/React Router v6 Complete in One Video/ch2/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
-import Post from './components/pages/Post';
 import Home from './components/pages/Home';
+import Post from './components/pages/Post';
+import PostDetail from './components/pages/PostDetail';
 import Layout from './components/Layout';
+import PostLayout from './components/PostLayout';
 
 function App() {
   return (
@@ -13,9 +15,13 @@ function App() {
           <Route path="/" element={<Layout />} >
             <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
-            <Route path='post' element={<Post />} />
+            <Route path='post' element={<PostLayout />} >
+              <Route path=":category" element={<PostDetail />} />
+              <Route index element={<Post />} />
+            </Route>
             <Route index element={<Home />} />
           </Route>
+          <Route path="*" element={<h1>Error 404 Page not Found !!</h1>} />
         </Routes>
       </BrowserRouter>
     </>
